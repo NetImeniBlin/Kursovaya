8f7f94352521e7f4c4ae47493a0beef0a239f462
Net Imeni <gagatushkan@gmail.com>
2021-12-15 18:29:51 +0500

    x

--- Form1.cs
+++ Form1.cs
@@ -31,8 +31,7 @@ namespace WindowsFormsApp4
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Form4 Form4 = new Form4();
-            Form4.ShowDialog();
+            
         }
     }
 }
--- potom.cs
+++ potom.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp4
 {
@@ -15,6 +16,43 @@ namespace WindowsFormsApp4
         public potom()
         {
             InitializeComponent();
+        }
+        MySqlConnection conn;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string file = "D:\\backup.sql";
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+                using (MySqlBackup mb = new MySqlBackup(cmd))
+                {
+                    cmd.Connection = conn;
+                    conn.Open();
+                    mb.ExportToFile(file);
+                    conn.Close();
+                }
+            }
+        }
+
+        private void potom_Load(object sender, EventArgs e)
+        {
+            string connStr = "server=caseum.ru;port=33333;user=st_2_8_19;database=st_2_8_19;password=46727777;";
+            conn = new MySqlConnection(connStr);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string file = "D:\\backup.sql";
+            using (MySqlCommand cmd = new MySqlCommand())
+            {
+                using (MySqlBackup mb = new MySqlBackup(cmd))
+                {
+                    cmd.Connection = conn;
+                    conn.Open();
+                    mb.ImportFromFile(file);
+                    conn.Close();
+                }
+            }
         }
     }
 }
