f70ebfc082ba7ff2a0825d3cd12c2af1b94d7a6d
Net Imeni <gagatushkan@gmail.com>
2021-12-20 23:41:47 +0500

    ура

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -30,7 +30,6 @@ namespace WindowsFormsApp4
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -40,67 +39,60 @@ namespace WindowsFormsApp4
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
-            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
-            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 28);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(275, 75);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(293, 28);
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(293, 54);
+            this.textBox2.Location = new System.Drawing.Point(12, 55);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
             this.textBox2.TabIndex = 2;
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(293, 80);
+            this.textBox3.Location = new System.Drawing.Point(12, 81);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(149, 20);
             this.textBox3.TabIndex = 4;
+            this.textBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox3_MouseClick);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 142);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 107);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(430, 296);
+            this.dataGridView1.Size = new System.Drawing.Size(438, 329);
             this.dataGridView1.TabIndex = 5;
             // 
             // toolStrip1
             // 
+            this.toolStrip1.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1,
             this.toolStripButton2,
             this.toolStripButton3,
             this.toolStripButton4,
-            this.toolStripLabel1,
-            this.toolStripLabel2,
-            this.toolStripLabel3,
-            this.toolStripLabel4});
+            this.toolStripComboBox1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(462, 25);
             this.toolStrip1.TabIndex = 6;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -132,51 +124,93 @@ namespace WindowsFormsApp4
             this.toolStripButton3.Name = "toolStripButton3";
             this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
             // 
             // toolStripButton4
             // 
+            this.toolStripButton4.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
             this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton4.Name = "toolStripButton4";
             this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
             // 
-            // toolStripLabel1
+            // toolStripComboBox1
             // 
-            this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
-            this.toolStripLabel1.Text = "toolStripLabel1";
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox1.TextChanged += new System.EventHandler(this.toolStripComboBox1_TextChanged);
             // 
-            // toolStripLabel2
+            // label1
             // 
-            this.toolStripLabel2.Name = "toolStripLabel2";
-            this.toolStripLabel2.Size = new System.Drawing.Size(86, 22);
-            this.toolStripLabel2.Text = "toolStripLabel2";
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(17, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ФИО нового сотрудника";
             // 
-            // toolStripLabel3
+            // label2
             // 
-            this.toolStripLabel3.Name = "toolStripLabel3";
-            this.toolStripLabel3.Size = new System.Drawing.Size(86, 22);
-            this.toolStripLabel3.Text = "toolStripLabel3";
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(17, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Возраст";
             // 
-            // toolStripLabel4
+            // label3
             // 
-            this.toolStripLabel4.Name = "toolStripLabel4";
-            this.toolStripLabel4.Size = new System.Drawing.Size(86, 22);
-            this.toolStripLabel4.Text = "toolStripLabel4";
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Window;
+            this.label3.Enabled = false;
+            this.label3.Location = new System.Drawing.Point(17, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Должность";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(338, 29);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(112, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox4_MouseClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.Enabled = false;
+            this.label4.Location = new System.Drawing.Point(344, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Новая должность";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(462, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form3";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
@@ -189,8 +223,6 @@ namespace WindowsFormsApp4
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
@@ -200,9 +232,11 @@ namespace WindowsFormsApp4
         private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.ToolStripButton toolStripButton3;
         private System.Windows.Forms.ToolStripButton toolStripButton4;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
-        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -18,6 +18,8 @@ namespace WindowsFormsApp4
         private DataSet ds = new DataSet();
         private DataTable table = new DataTable();
         string id_selected_rows = "0";
+        string SelectedTable;
+        string commandStr;
 
         public Form3()
         {
@@ -31,12 +33,12 @@ namespace WindowsFormsApp4
             string index_selected_rows;
             index_selected_rows = dataGridView1.SelectedCells[0].RowIndex.ToString();
             id_selected_rows = dataGridView1.Rows[Convert.ToInt32(index_selected_rows)].Cells[0].Value.ToString();
-            toolStripLabel4.Text = id_selected_rows;
         }
 
         public void GetListUsers()
         {
-            string commandStr = "SELECT id, FIO, age, dolg FROM сотрудники1";
+            SelectedTable = Convert.ToString(toolStripComboBox1.Text);
+            commandStr = $"SELECT id, FIO, age, dolg FROM {SelectedTable}";
             conn.Open();
             MyDA.SelectCommand = new MySqlCommand(commandStr, conn);
             MyDA.Fill(table);
@@ -44,21 +46,42 @@ namespace WindowsFormsApp4
             dataGridView1.DataSource = bSource;
             conn.Close();
             int count_rows = dataGridView1.RowCount - 1;
-            toolStripLabel2.Text = (count_rows).ToString();
         }
 
         public void reload_list()
         {
             table.Clear();
             GetListUsers();
+            ChangeColorDGV();
         }
 
-        private void Form3_Load(object sender, EventArgs e)
+        private void ChangeColorDGV()
         {
-            string connStr = "server=caseum.ru;port=33333;user=st_2_8_19;database=st_2_8_19;password=46727777;";
-            conn = new MySqlConnection(connStr);
-            GetListUsers();
-         //   GetListSotrudniki(listBox1);
+            int count_rows = dataGridView1.RowCount - 1;
+            for (int i = 0; i < count_rows; i++)
+            {
+                string dolgstatus = Convert.ToString(dataGridView1.Rows[i].Cells[3].Value);
+                if (dolgstatus == "стажер")
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Yellow;
+                }
+                else if (dolgstatus == "уволен")
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+                }
+                else
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Green;
+                }
+            }
+        }
+
+        public void items()
+        {
+            toolStripComboBox1.Items.Add("сотрудники1");
+            toolStripComboBox1.Items.Add("сотрудники2");
+            toolStripComboBox1.Items.Add("сотрудники3");
+            toolStripComboBox1.Text = "сотрудники1";
         }
 
         public bool InsertSotrudniki(string Ifio, string Iage, string Idolg)
@@ -66,7 +89,7 @@ namespace WindowsFormsApp4
             int InsertCount = 0;
             bool result = false;
             conn.Open();
-            string query = $"INSERT INTO сотрудники1 (FIO, age, dolg) VALUES ('{Ifio}', '{Iage}', '{Idolg}')";
+            string query = $"INSERT INTO {SelectedTable} (FIO, age, dolg) VALUES ('{Ifio}', '{Iage}', '{Idolg}')";
             try
             {
                 MySqlCommand command = new MySqlCommand(query, conn);
@@ -87,19 +110,14 @@ namespace WindowsFormsApp4
             return result;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form3_Load(object sender, EventArgs e)
         {
-            string Ifio = textBox1.Text;
-            string Iage = textBox2.Text;
-            string Idolg = textBox3.Text;
-            if (InsertSotrudniki(Ifio, Iage, Idolg))
-            {
-                reload_list();
-            }
-            else
-            {
-                MessageBox.Show("Произошла ошибка.", "Ошибка");
-            }
+            string connStr = "server=caseum.ru;port=33333;user=st_2_8_19;database=st_2_8_19;password=46727777;";
+            conn = new MySqlConnection(connStr);
+            ToolTip tip = new ToolTip();
+            items();
+            GetListUsers();
+            ChangeColorDGV();
         }
 
         private void toolStripButton2_Click(object sender, EventArgs e)
@@ -130,6 +148,59 @@ namespace WindowsFormsApp4
         {
             reload_list();
             MessageBox.Show("обновлено","status");
+        }
+
+        private void toolStripComboBox1_TextChanged(object sender, EventArgs e)
+        {
+            SelectedTable = Convert.ToString(toolStripComboBox1.Text);
+            reload_list();
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            label2.Visible = false;
+        }
+
+        private void textBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            label3.Visible = false;
+        }
+
+        private void textBox4_MouseClick(object sender, MouseEventArgs e)
+        {
+            label4.Visible = false;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            string Ifio = textBox1.Text;
+            string Iage = textBox2.Text;
+            string Idolg = textBox3.Text;
+            if (InsertSotrudniki(Ifio, Iage, Idolg))
+            {
+                reload_list();
+            }
+            else
+            {
+                MessageBox.Show("Произошла ошибка.", "Ошибка");
+            }
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            GetSelectedIDString();
+            string newdolg = Convert.ToString(textBox4.Text);  
+            string cm = ($"UPDATE {SelectedTable} SET dolg='{newdolg}' WHERE id='{id_selected_rows}'");
+            MySqlCommand redact = new MySqlCommand(cm, conn);
+            conn.Open();
+            redact.ExecuteNonQuery();
+            conn.Close();
+            reload_list();
         }
     }
 }
